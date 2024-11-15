0756aa154fa4dc0ef37a9b0f5d914cddb5cb1758
madesugiyanti <madeyantisugi19@gmail.com>
2024-11-15 16:39:02 +0800

    Add files via upload

---
+++ AdministrasiKependudukan/src/administrasikependudukan/AdministrasiKependudukan.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package administrasikependudukan;
+import config2210010052_pbo2.configDB;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author madey
+ */
+public class AdministrasiKependudukan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+//         configDB objekku = new configDB();
+//          if(objekku.DuplicateKey("ektp", "nik", "62035466")){
+//            JOptionPane.showMessageDialog(null,"Maaf, data sudah ada ");
+//        } else{
+//            JOptionPane.showMessageDialog(null,"Data berhasil ditambahkan ");
+//        }    
+//     
+       new gui.mainFrame().setVisible(true);
+
+   }
+    
+}
